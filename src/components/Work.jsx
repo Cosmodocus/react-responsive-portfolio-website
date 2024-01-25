d21cac8aed8786a1f97c1700f081487a94e578a2
Cosmodocus <149424191+Cosmodocus@users.noreply.github.com>
2024-01-25 16:54:06 -0500

    Work.jsx

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -4,7 +4,7 @@ import realEstate from '../assets/projects/realEstate.jpg'
 
 const Work = () => {
   return (
-    <div name='work' className='bg-[#0a192f] w-full md:h-screen text-gray-300 py-20' style={{ borderBottom: '2px solid #8892b0' }}>
+    <div name='work' className='bg-[#0a192f] w-full text-gray-300 py-20' style={{ borderBottom: '2px solid #8892b0' }}>
         <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
             <div className='pb-8'>
                 <p className='text-4xl font-bold inline border-b-4 text-gray-300 border-[#00b894] '>Work</p>
