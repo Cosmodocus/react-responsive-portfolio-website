7e1e7f98fab9a2021b987835c3236e434e24beef
Cosmodocus <149424191+Cosmodocus@users.noreply.github.com>
2024-01-25 16:55:51 -0500

    Contact.jsx

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -8,11 +8,11 @@ const Contact = () => {
                 <p className='text-4xl font-bold inline border-b-4 border-[#00b894]  text-gray-300'>Contact</p>
                 <p className='text-gray-300 py-4'>// Submit the form below or shoot me an email - myemail@gmail.com</p>
             </div>
-            <input className='bg-[#ccd6f6] p-2' type="text" placeholder='Name' name='name' />
-            <input className='my-4 p-2 bg-[#ccd6f6]' type="email" placeholder='Email' name='email' />
-            <textarea className='bg-[#ccd6f6] p-2'
+            <input className='bg-[#ccd6f6] p-2 rounded-2xl' type="text" placeholder='Name' name='name' />
+            <input className='my-4 p-2 bg-[#ccd6f6] rounded-2xl' type="email" placeholder='Email' name='email' />
+            <textarea className='bg-[#ccd6f6] p-2 rounded-xl'
             name="message" rows="10" placeholder='Message'></textarea>
-            <button className='text-white border-2 hover:bg-[#00b894]  hover:border-[#00b894]  px-4 py-3 mx-auto flex items-center my-8'>Let's Collaborate</button>
+            <button className='text-white border-2 hover:bg-[#00b894] rounded-2xl hover:border-[#00b894]  px-4 py-3 mx-auto flex items-center my-8'>Let's Collaborate</button>
         </form>
     </div>
   )
