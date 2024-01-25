19d50207356d586a2ae06ad5fe4f5d1cea219f92
Cosmodocus <149424191+Cosmodocus@users.noreply.github.com>
2024-01-25 16:53:45 -0500

    Skills.jsx

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -10,7 +10,7 @@ import Mongo from '../assets/mongo.png';
 
 const Skills = () => {
   return (
-    <div name='skills' className='py-20 bg-[#0a192f] text-gray-300 w-full ' style={{ borderBottom: '2px solid #8892b0' }}>
+    <div name='skills' className='py-20 bg-[#0a192f] text-gray-300 w-full h-screen' style={{ borderBottom: '2px solid #8892b0' }}>
         {/* CONTAINER */}
         <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'> 
             <div>
