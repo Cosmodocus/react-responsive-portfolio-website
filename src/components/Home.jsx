08bf0b3ae6d7ce6604082cdb4f5fdc9e3bc96e33
Cosmodocus <149424191+Cosmodocus@users.noreply.github.com>
2024-01-25 19:34:09 -0500

    Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,7 +3,7 @@ import {HiArrowNarrowRight} from 'react-icons/hi'
 
 const Home = () => {
   return (
-    <div name='home' className='w-full h-screen bg-[#0a192f] border-b-2 border-solid border-[#8892b0] sm:border-b-0 md:border-b-2 lg:border-b-2'>
+    <div name='home' className='w-full h-screen bg-[#0a192f] border-b-2 border-solid border-[#8892b0]'>
         {/* Container */}
         <div className='max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full '>
             <p className='text-[#00b894]'>Hi, my name is</p>
