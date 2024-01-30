79c2c2f36194e74b916f736ee3a2d4c01f33deee
Cosmodocus <149424191+Cosmodocus@users.noreply.github.com>
2024-01-30 18:38:45 -0500

    Update Contact.jsx
    updated text and styling

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -6,11 +6,11 @@ const Contact = () => {
         <form method='POST' action="https://getform.io/f/ce66e89a-16f3-40dd-8203-ad8630371ad6" className='flex flex-col max-w-[600px] w-full'>
             <div className='pb-8'>
                 <p className='text-4xl font-bold inline border-b-4 border-[#00b894]  text-gray-300'>Contact</p>
-                <p className='text-gray-300 py-4'>// Submit the form below or shoot me an email - myemail@gmail.com</p>
+                <p className='text-gray-300 py-4 font-bold'>Submit the form below to contact me — or reach me at homayunir@gmail.com</p>
             </div>
-            <input className='bg-[#ccd6f6] p-2 rounded-2xl' type="text" placeholder='Name' name='name' />
-            <input className='my-4 p-2 bg-[#ccd6f6] rounded-2xl' type="email" placeholder='Email' name='email' />
-            <textarea className='bg-[#ccd6f6] p-2 rounded-xl'
+            <input className='bg-[#ccd6f6] p-2 rounded-2xl font-bold' type="text" placeholder='Name' name='name' />
+            <input className='my-4 p-2 bg-[#ccd6f6] rounded-2xl font-bold' type="email" placeholder='Email' name='email' />
+            <textarea className='bg-[#ccd6f6] p-2 rounded-xl font-bold'
             name="message" rows="10" placeholder='Message'></textarea>
             <button className='text-white border-2 hover:bg-[#00b894] rounded-2xl hover:border-[#00b894]  px-4 py-3 mx-auto flex items-center my-8'>Let's Collaborate</button>
         </form>
