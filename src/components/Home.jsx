4b67e70189cf7ab65cc13144e8eb8879c5a431f3
Cosmodocus <149424191+Cosmodocus@users.noreply.github.com>
2024-01-30 18:38:23 -0500

    Update Home.jsx
    Added react-typed text animations

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import {HiArrowNarrowRight} from 'react-icons/hi'
+import { ReactTyped } from "react-typed";
 
 const Home = () => {
   return (
@@ -8,10 +9,18 @@ const Home = () => {
         <div className='max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full '>
             <p className='text-[#00b894]'>Hi, my name is</p>
             <h1 className='text-4xl sm:text-7xl font-bold text-[#ccd6f6]'>Reza Homayuni</h1>
-            <h2 className='text-4xl sm:text-7xl font-bold text-[#8892b0]'>I'm a Frontend Developer</h2>
+            <h2 className='text-4xl sm:text-7xl font-bold text-[#8892b0]'>
+              I'm a <ReactTyped 
+                    strings={['Frontend Developer']}
+                    typeSpeed={80}
+                    backSpeed={120}
+                    loop
+                    className ='text-4xl sm:text-7xl font-bold text-[#00b894]'
+                    />
+            </h2>
             <p className='text-[#8892b0] py-4 max-w-[700px]'>I'm a frontend developer specializing in building and occasionally designing exceptional digital experiences. Currently, I'm focused on building responsive frontend web applications.</p>
             <div>
-                <button className='text-white group border-2 rounded-2xl px-6 py-3 my-2 flex items-center hover:bg-[#00b894]  hover:border-[#00b894] '>View Work 
+                <button className='text-white group border-2 rounded-2xl px-6 py-3 my-2 flex items-center hover:bg-[#00b894]  hover:border-[#00b894] ' onclick="window.location.href='https://www.example.com'">View Work 
                 <span className='group-hover:rotate-90 duration-300'>
                     <HiArrowNarrowRight className='ml-3'/></span>
                 </button>
