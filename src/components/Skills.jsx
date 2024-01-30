67376935dd9293dec8c62a2c6ebcf0ccceec87e2
Cosmodocus <149424191+Cosmodocus@users.noreply.github.com>
2024-01-30 18:40:40 -0500

    Update Skills.jsx
    Updated skills to match my concurrent skills, and updated their responsiveness as well

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -15,42 +15,42 @@ const Skills = () => {
         <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'> 
             <div>
                 <p className='text-4xl font-bold inline border-b-4 border-[#00b894] '>Skills</p>
-                <p className='py-4'>// These are the technologies I've worked with</p>
+                <p className='py-4 font-bold'>These are all the skills I'm experienced with:</p>
             </div>
 
-            <div className='w-full grid grid-cols-2 sm:grid-cols-4 gap-10 text-center py-8'>
-                <div className='shadow-md shadow-[#040c16] hover:scale-125 duration-500'>
+            <div className='w-full grid grid-cols-1 sm:grid-cols-3 gap-12 text-center py-8'>
+                <div className='shadow-md shadow-[#040c16] hover:scale-125 duration-500 p-2'>
                     <img className='w-20 mx-auto' src={HTML} alt="HTML icon" />
-                    <p className='my-4'>HTML</p>
+                    <p className='my-5'>HTML</p>
                 </div>
-                <div className='shadow-md shadow-[#040c16] hover:scale-125 duration-500'>
+                <div className='shadow-md shadow-[#040c16] hover:scale-125 duration-500 p-2'>
                     <img className='w-20 mx-auto' src={CSS} alt="HTML icon" />
-                    <p className='my-4'>CSS</p>
+                    <p className='my-5'>CSS</p>
                 </div>
-                <div className='shadow-md shadow-[#040c16] hover:scale-125 duration-500'>
+                <div className='shadow-md shadow-[#040c16] hover:scale-125 duration-500 p-2'>
                     <img className='w-20 mx-auto' src={Tailwind} alt="HTML icon" />
-                    <p className='my-4'>Tailwind CSS</p>
+                    <p className='my-5'>Tailwind CSS</p>
                 </div>
-                <div className='shadow-md shadow-[#040c16] hover:scale-125 duration-500'>
+                <div className='shadow-md shadow-[#040c16] hover:scale-125 duration-500 p-2'>
                     <img className='w-20 mx-auto' src={JavaScript} alt="HTML icon" />
-                    <p className='my-4'>JavaScript</p>
+                    <p className='my-5'>JavaScript</p>
                 </div>
-                <div className='shadow-md shadow-[#040c16] hover:scale-125 duration-500'>
+                <div className='shadow-md shadow-[#040c16] hover:scale-125 duration-500 p-2'>
                     <img className='w-20 mx-auto' src={ReactImg} alt="HTML icon" />
-                    <p className='my-4'>React</p>
+                    <p className='my-5'>React</p>
                 </div>
-                <div className='shadow-md shadow-[#040c16] hover:scale-125 duration-500'>
+                <div className='shadow-md shadow-[#040c16] hover:scale-125 duration-500 p-2'>
                     <img className='w-20 mx-auto' src={GitHub} alt="HTML icon" />
-                    <p className='my-4'>GitHub</p>
+                    <p className='my-5'>GitHub</p>
                 </div>
-                <div className='shadow-md shadow-[#040c16] hover:scale-125 duration-500'>
+                {/* <div className='shadow-md shadow-[#040c16] hover:scale-125 duration-500'>
                     <img className='w-20 mx-auto' src={Node} alt="HTML icon" />
                     <p className='my-4'>Node Js</p>
                 </div>
                 <div className='shadow-md shadow-[#040c16] hover:scale-125 duration-500'>
                     <img className='w-20 mx-auto' src={Mongo} alt="HTML icon" />
                     <p className='my-4'>Mongo DB</p>
-                </div>
+                </div> */}
                 
             </div>
         </div>
